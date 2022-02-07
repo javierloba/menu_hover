5056e91b25826079af9ce78f85ba6886ea528952
Javier Loba <30801844+javierloba@users.noreply.github.com>
2022-02-07 11:15:03 +0100

    Hover effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <ul>
+        <li><a href='#' data-text="Home">Home</a></li>
+        <li><a href='#' data-text="About">About</a></li>
+        <li><a href='#' data-text="Services">Services</a></li>
+        <li><a href='#' data-text="Team">Team</a></li>
+        <li><a href='#' data-text="Contact">Contact</a></li>
+      </ul>
+    </section>
   );
 }
 
